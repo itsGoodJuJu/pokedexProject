4156bb26c8555cc46901ff606e3bffa52e59b85c
COR Coding <ls>
2024-04-10 11:40:37 -0400

    Creating app.delete (IN-PROGRESS)

--- npm/index.js
+++ npm/index.js
@@ -37,6 +37,25 @@ await db.any('SELECT * FROM pokedex')
 
 // David DELETE
 
+app.delete('/pokedex/:id', (req, res)=> {
+    if(Object.keys(req.body).length != 0) {
+        res.status(400).json({error: "Request within the body is not allowed"}); 
+
+    } else if(Object.keys(req.query).length > 1){ 
+        clientError(req, "Query Parameters do not meet requirements", 400);
+        res.status(400).json({error: "Query Parameters do not meet requirements"});
+
+    } else if((Object.keys(req.query).length != 0) && (Object.keys(req.query)[0] != "id")){
+        clientError(req, "Query Parameters do not meet requirements", 400);
+        res.status(401).json({error: "Query Parameters do not meet requirements"});
+
+    } else if(isNaN(req.query.id) && req.query.id != undefined){
+        clientError(req, "id provided is not a number", 400);
+        res.status(401).json({error: "id provided is not a number"});
+    }
+})
+
+
 
 app.listen(5432, () => {
     console.log("Server is running on port 5432");
